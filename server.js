2d68ad0b00e185519ff35732c46f21fb530ddb94
Airwoman <airwomanzhy@hotmail.com>
2017-07-19 23:19:20 +1000

    set controller and reset server add body-parser

--- server.js
+++ server.js
@@ -1,6 +1,18 @@
 var express = require('express'),
   app = express(),
-  port = process.env.PORT || 3333;
+  port = process.env.PORT || 3333,
+  mongoose = require('mongoose'),
+  Task = require('./api/models/petStoreModel'),
+  bodyParser = require('body-parser');
+
+mongoose.Promise = global.Promise;
+mongoose.connect('mongodb://localhost/PetStoredb');
+
+app.use(bodyParser.urlencoded({ extended: true }));
+app.use(bodyParser.json());
+
+var routes = require('./api/routes/petStoreRoutes');
+routes(app);
 
 app.listen(port);
 
